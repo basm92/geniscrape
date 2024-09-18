a021b09a9f8f8f550e1b0a70d02f36f8ff7598bc
MCPdeHaan <mauritsdehaan.info@gmail.com>
2024-09-18 15:57:22 +0200

    update roxyugen note and no test in wie was wei find cross section

--- R/wiewaswie_find_cross_section.R
+++ R/wiewaswie_find_cross_section.R
@@ -92,6 +92,3 @@ wiewaswie_find_cross_section <- function(place, year, sleep_time=1, type="Geboor
   return(out)
 
 }
-
-# example:
-test <- wiewaswie_find_cross_section("Hellendoorn", "1850", type="Overlijden")
